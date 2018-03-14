b718e0c16555c57491df663e2110e8718d80a7cc
Bernardo Copstein <bernardo.copstein@pucrs.br>
2018-03-14 09:40:42 -0300

    exercicio observer

---
+++ Topico1_ArquiteturaDeSoftware/Exercicio1_ImplementacaoSistema/Ver2_Singleton/Operacoes.java
@@ -0,0 +1,50 @@
+package Refactoring01_Singleton_Iterator;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class Operacoes {
+    public static Operacoes operacoesInstancia = null;
+	private List<Operacao> operacoes; 
+	private ObservableList<Operacao> operacoesContaEmUso;
+
+	private Operacoes() {
+		operacoes = Persistencia.getInstance().loadOperacoes();
+	}
+	
+	public static Operacoes getInstance() {
+		if (operacoesInstancia == null) {
+			operacoesInstancia = new Operacoes();
+		}
+		return operacoesInstancia;
+	}
+
+	public void saveOperacoes() {
+		Persistencia.getInstance().saveOperacoes(operacoes);
+	}
+    
+	public ObservableList<Operacao> setContaEmUso() {
+		if (Contas.getInstance().getContaEmUso() == null) {
+			return null;
+		}else {
+			Conta conta = Contas.getInstance().getContaEmUso();
+			operacoesContaEmUso = 
+	        		FXCollections.observableArrayList(
+	        				operacoes
+	        				.stream()
+	        				.filter(op -> op.getNumeroConta() == Contas.getInstance().getContaEmUso().getNumero())
+	        				.collect(Collectors.toList())
+	        				);
+            return operacoesContaEmUso;
+		}
+		
+	}
+	
+	public void insereNovaOperacao(Operacao op) {
+		operacoes.add(op);
+		operacoesContaEmUso.add(op);
+	}
+}
